0bf0054421355c6fa633357b4dcf75a0f3e38d28
Ox0spy <Ox0spy@gmail.com>
2011-03-03 21:47:21 +0800

    merge the answer of the level 7, now 7.py can get the answer.

--- 7-2.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-a = [105, 110, 116, 101, 103, 114, 105, 116, 121]
-a = [chr(x) for x in a]
-print ''.join(a)
--- 7.py
+++ 7.py
@@ -9,6 +9,7 @@ png_fname = 'oxygen.png'
 png_url = 'http://www.pythonchallenge.com/pc/def/oxygen.png'
 if not os.path.exists(png_fname):
     fout = open(png_fname, 'w')
+    print 'downloading %s, please wait ...' % png_fname
     fout.write(urllib2.urlopen(png_url).read())
     fout.close()
 
@@ -16,8 +17,15 @@ img = Image.open(png_fname, 'r')
 res = []
 for h in range(img.size[1]):
     for w in range(img.size[0]):
-        if h == 43 and (w+1) % 7 == 0:
+        if h == 43 and w % 7 == 0:
             res.append(img.getpixel((w,h))[0])
 
 res = [chr(x) for x in res]
-print ''.join(res)
+msg= ''.join(res)
+print msg
+
+start, end = (msg.find('['), msg.find(']'))
+nmsg = msg[start:end+1]
+nmsg = [105, 110, 116, 101, 103, 114, 105, 116, 121]
+nmsg = [chr(x) for x in nmsg]
+print ''.join(nmsg)

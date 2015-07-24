196d888414b76fd010190467a0bd309bf61bbdb8
ox0spy <ox0spy@gmail.com>
2015-07-24 22:42:51 +0800

    rewrite answer for question 1.

--- 1.py
+++ 1.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
+# encoding: utf-8
 
-import sys
+import string
 
-s = sys.argv[1]
-r = ''
+s = r"g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+url = r"http://www.pythonchallenge.com/pc/def/map.html"
 
-for x in s:
-    r = r + chr(ord(x)+2)
+def tran(s):
+    sl = [chr(ord(x)+2) if x in string.ascii_letters else x for x in s]
+    ss = ''.join(sl)
+    table = string.maketrans('{|', 'ab')
+    return string.translate(ss, table)
 
-print r
+print tran(s)
+print
+surl = tran(url.split('/')[-1].split('.')[0])
+suf = url.split('.')[-1]
+print '/'.join(url.split('/')[:-1]) + '/' + surl + '.' + suf

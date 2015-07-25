194c2f7c38d4cd7b972c9e1e9a638d75ed99b1e2
ox0spy <ox0spy@gmail.com>
2015-07-25 22:47:55 +0800

    modify 3.sh.

--- 3.sh
+++ 3.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-grep -Po '[^A-Z][A-Z]{3}([a-z])[A-Z]{3}[^A-Z]' 3 | cut -c5 | xargs | tr -d ' '
+url='http://www.pythonchallenge.com/pc/def/equality.html'
+curl -s $url | sed -ne '/<!--/,/-->/p' | \
+    grep -Eo '[^A-Z][A-Z]{3}([a-z])[A-Z]{3}[^A-Z]' | \
+    cut -c5 | xargs | tr -d ' '

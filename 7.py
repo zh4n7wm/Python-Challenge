bf25f70fb58c31f25dc784dfb1b3b9a71eef4372
Zhang Wanming <wanming.zhang@tcl.com>
2015-07-28 11:11:13 +0800

    modify 7.py, using print_function.

--- 7.py
+++ 7.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from __future__ import print_function
 import urllib2
 from StringIO import StringIO
 from PIL import Image
 
-png_fname = 'oxygen.png'
+# url = 'http://www.pythonchallenge.com/pc/def/oxygen.html'
 png_url = 'http://www.pythonchallenge.com/pc/def/oxygen.png'
-print 'downloading %s, please wait ...' % png_fname
+print('loading image, please wait ...')
 fimg = StringIO(urllib2.urlopen(png_url).read())
 
 img = Image.open(fimg)
-w = img.size[0] # width
-h = img.size[1] # heigh
-m = h/2         # middle
+w = img.size[0]  # width
+h = img.size[1]  # heigh
+m = h / 2        # middle
 res = []
 for i in range(0, w, 7):
     p = img.getpixel((i, m))  # note: m in m-4 ~ m+4 is OK
@@ -23,8 +24,8 @@ img.close()
 
 res = [chr(x) for x in res]
 msg= ''.join(res)
-print msg
+print(msg)
 
 start, end = (msg.find('['), msg.find(']'))
-nmsg = msg[start+1:end].split(',')
-print ''.join([chr(int(x)) for x in nmsg])
+nmsg = msg[start + 1:end].split(',')
+print(''.join([chr(int(x)) for x in nmsg]))

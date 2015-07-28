6db97264d82166a186560d80616e53397891c194
Zhang Wanming <wanming.zhang@tcl.com>
2015-07-28 11:07:04 +0800

    modify 7.py

--- 7.py
+++ 7.py
@@ -11,11 +11,14 @@ print 'downloading %s, please wait ...' % png_fname
 fimg = StringIO(urllib2.urlopen(png_url).read())
 
 img = Image.open(fimg)
+w = img.size[0] # width
+h = img.size[1] # heigh
+m = h/2         # middle
 res = []
-for h in range(img.size[1]):
-    for w in range(img.size[0]):
-        if h == 43 and w % 7 == 0:
-            res.append(img.getpixel((w,h))[0])
+for i in range(0, w, 7):
+    p = img.getpixel((i, m))  # note: m in m-4 ~ m+4 is OK
+    if p[0] == p[1] == p[2]:  # also can img.convert("L") first
+        res.append(p[0])
 img.close()
 
 res = [chr(x) for x in res]

64d7656cc93ac3607e436496d458857b75c0c7bf
Zhang Wanming <wanming.zhang@tcl.com>
2015-07-29 15:50:33 +0800

    add 11-n.py

---
+++ 11-n.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+level 11: http://www.pythonchallenge.com/pc/return/5808.html
+"""
+
+from StringIO import StringIO
+from PIL import Image
+import requests
+
+img_url = 'http://www.pythonchallenge.com/pc/return/cave.jpg'
+fimg = StringIO(requests.get(img_url, auth=('huge', 'file')).content)
+im = Image.open(fimg)
+data = list(im.getdata())
+even = data[::2]
+odd = data[1::2]
+even_im = Image.new(im.mode, im.size)
+even_im.putdata(even)
+even_im.show()
+
+odd_im = Image.new(im.mode, im.size)
+odd_im.putdata(odd)
+odd_im.show()

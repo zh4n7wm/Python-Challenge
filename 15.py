a6fef6552b7f4d0c1d2c15bd75f89d0acc877448
ox0spy <ox0spy@gmail.com>
2015-07-31 15:44:32 +0800

    modify 15.py

--- 15.py
+++ 15.py
@@ -1,3 +1,33 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+level15: http://www.pythonchallenge.com/pc/return/uzi.html
+TODO:
+1.Python datetime, calendar 和Linux date, cal 输出结果不一致.
+如:
+
+In [40]: import calendar
+In [41]: c = calendar.TextCalendar(calendar.SUNDAY)
+In [42]: c.prmonth(1036, 1)
+    January 1036
+Su Mo Tu We Th Fr Sa
+                1  2
+ 3  4  5  6  7  8  9
+10 11 12 13 14 15 16
+17 18 19 20 21 22 23
+24 25 26 27 28 29 30
+31
+
+In [43]: !cal 1 1036
+    January 1036
+Su Mo Tu We Th Fr Sa
+             1  2  3
+ 4  5  6  7  8  9 10
+11 12 13 14 15 16 17
+18 19 20 21 22 23 24
+25 26 27 28 29 30 31
+"""
+
 from calendar import isleap
 import calendar
 

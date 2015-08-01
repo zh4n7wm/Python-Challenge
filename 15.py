49658ac9cdbe8be049d5c9d8b2a838d64cbbcf9d
ox0spy <ox0spy@gmail.com>
2015-08-01 10:36:54 +0800

    modify 15.py, switch print_function.

--- 15.py
+++ 15.py
@@ -28,9 +28,10 @@ Su Mo Tu We Th Fr Sa
 25 26 27 28 29 30 31
 """
 
+from __future__ import print_function
 from calendar import isleap
 import calendar
 
 for year in xrange(1006, 2000, 10):
     if isleap(year) and calendar.weekday(year, 1, 27) == 1:
-        print year
+        print(year)
